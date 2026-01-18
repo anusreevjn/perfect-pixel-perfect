51f1127f758b2ee1d4dea9ee97797c89e964210e
anusreevjn <144801382+anusreevjn@users.noreply.github.com>
2026-01-18 18:07:54 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/perfect-pixel-perfect/", // <--- ADD THIS LINE (use your actual repo name)
+  base: "/perfect-pixel-perfect/", // <--- ADD THIS (Must match your repo name exactly)
   server: {
     host: "::",
     port: 8080,
